05ddb6e8fbf62370369bb6cb2e0ca1c2f33636d9
technifutur <technifutur@DESKTOP-EHN422D>
2023-03-09 11:07:48 +0100

    demo methodes

--- Devops.Demo/DemoMethodes.cs
+++ Devops.Demo/DemoMethodes.cs
@@ -18,31 +18,84 @@ namespace Devops.Demo
             int[] tab1 = new int[taille];
             int[,] tab2 = new int[taille, taille];
             FillTab(tab1);
+            FillTab(tab2);
             ShowTab(tab1);
-            tab1[2] = 5;
+            Console.WriteLine("_____________________________________________");
+            ShowTab(tab2);
+            Console.WriteLine("_____________________________________________");
+            int nb = 5;
+            Console.WriteLine(nb);
+            ChangeValue(ref nb);
+            Console.WriteLine(nb);
+            Console.WriteLine("_____________________________________________");
+            ChangeValue(tab1);
             ShowTab(tab1);
-            tab1[6] = 8;
-            ShowTab(tab1);
-            tab1[0] = 20;
-            ShowTab(tab1);
+            Console.WriteLine(Addition(2, 6, 5, 7, 8, 9, 7));
+            Console.WriteLine(estDivisible(5));
+            Console.WriteLine(estDivisible(6, 3));
         }
-
-        private void FillTab(int[] tab1)
+        public bool estDivisible(int nb,int div = 2)
+        {
+            return nb% div == 0;
+        }
+        public int Addition(params int[] tab)
+        {
+            int somme = 0;
+            foreach (int nb in tab)
+            {
+                somme += nb;
+            }
+            return somme;
+        }
+        public void ChangeValue(ref int i)
+        {
+            i += 10;
+        }
+        public void ChangeValue(int[] tab)
+        {
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] += 10;
+            }    
+        }
+        private void FillTab(int[] tab)
         {
             int cpt = 1;
-            for (int i = 0; i < tab1.Length; i++)
+            for (int i = 0; i < tab.Length; i++)
             {
-                tab1[i] = cpt++;
+                tab[i] = cpt++;
+            }
+        }
+        private void FillTab(int[,] tab)
+        {
+            int cpt = 1;
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                for (int j = 0; j < tab.GetLength(1); j++)
+                {
+                    tab[i, j] = cpt++;
+                }
             }
         }
 
-        private static void ShowTab(int[] tab1)
+        private static void ShowTab(int[] tab)
         {
-            for (int i = 0; i < tab1.Length; i++)
+            for (int i = 0; i < tab.Length; i++)
             {
-                Console.Write(tab1[i] + " | ");
+                Console.Write(tab[i] + " | ");
             }
             Console.WriteLine();
+        }
+        private static void ShowTab(int[,] tab)
+        {
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                for (int j = 0; j < tab.GetLength(1); j++)
+                {
+                    Console.Write(tab[i, j] + " | ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
